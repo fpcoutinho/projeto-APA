d629e511a2589f62dc9c7b8f79443876fb9a14aa
joao <joaocandido@cc.ci.ufpb.br>
2021-11-28 12:34:30 -0300

    matriz completa2 como funcao

--- projeto.py
+++ projeto.py
@@ -7,16 +7,19 @@ def criarMatriz():
         for c in range(numConvidados):
             if(l < c):
                 matriz[l][c] = randint(-9,9)
+                matriz[c][l] = matriz[l][c]
+                print(matriz[l][c], end="\t")                
             elif(l == c):
                 matriz[l][c] = "-"
+                print(matriz[l][c], end="\t")                
             else:
-                matriz[l][c] = " "
-            print(matriz[l][c], end="\t")
+                print(" \t", end="")
             
         print()
-
-
         
 numConvidados = int(input('Quantos convidados? '))
+# numMesas =  int(input('Quantas mesas? '))
+# limMin =  int(input('Qual o mínimo de pessoas que devem sentar em cada mesa? '))
+# limMax =  int(input('E qual o máximo? '))
 
 criarMatriz()
