1af3ab4e6504866215a926aeaa74e7a790b2b8d4
joao <joaocandido@cc.ci.ufpb.br>
2021-11-28 10:56:25 -0300

    matriz completa como funcao

--- projeto.py
+++ projeto.py
@@ -1,17 +1,22 @@
 from random import randint
 
+def criarMatriz():
+    matriz = [[0 for _ in range(numConvidados)] for _ in range(numConvidados)]
+    
+    for l in range(numConvidados):
+        for c in range(numConvidados):
+            if(l < c):
+                matriz[l][c] = randint(-9,9)
+            elif(l == c):
+                matriz[l][c] = "-"
+            else:
+                matriz[l][c] = " "
+            print(matriz[l][c], end="\t")
+            
+        print()
+
+
+        
 numConvidados = int(input('Quantos convidados? '))
 
-matriz = [[0 for _ in range(numConvidados)] for _ in range(numConvidados)]
-    
-for l in range(numConvidados):
-    for c in range(numConvidados):
-        if(l < c):
-            matriz[l][c] = randint(-9,9)
-        elif(l == c):
-            matriz[l][c] = "-"
-        else:
-            matriz[l][c] = " "
-        print(matriz[l][c], end="\t")
-        
-    print()
+criarMatriz()
