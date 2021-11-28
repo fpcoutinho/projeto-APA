740733e06ba14d2e2f4c1ba284d2df1b16d91504
joao <joaocandido@cc.ci.ufpb.br>
2021-11-28 10:16:33 -0300

    matriz completa

--- projeto.py
+++ projeto.py
@@ -1,10 +1,11 @@
 from random import randint
 
-numConvidados = int(input("Quantos Convidados?\n"))
+numConvidados = int(input('Quantos convidados? '))
 
-matrix = []
+matriz = [[0 for _ in range(numConvidados)] for _ in range(numConvidados)]
+    
 for l in range(numConvidados):
     for c in range(numConvidados):
-	    matrix.append(randint(-9,9))
- 
-print(matrix)
+        matriz[l][c] = randint(-9,9)
+        
+print(matriz)
