3929c9251ec7aedf1659bc859a5bd2cc5ab85e63
Filipe Coutinho <fpcoutinho5@gmail.com>
2021-11-29 16:09:47 -0300

    separou função de printar da função de criar a matriz.

--- projeto.py
+++ projeto.py
@@ -1,6 +1,6 @@
 from random import randint
 
-def criarMatriz():
+def criarMatriz(numConvidados):
     matriz = [[0 for _ in range(numConvidados)] for _ in range(numConvidados)]
     
     for l in range(numConvidados):
@@ -8,18 +8,26 @@ def criarMatriz():
             if(l < c):
                 matriz[l][c] = randint(-9,9)
                 matriz[c][l] = matriz[l][c]
-                print(matriz[l][c], end="\t")                
             elif(l == c):
                 matriz[l][c] = "-"
-                print(matriz[l][c], end="\t")                
+    return matriz
+
+def printMatriz(matriz):
+    for l in range(len(matriz)):
+        for c in range(len(matriz)):
+            if (l < c):
+                print(matriz[l][c], end="\t")
+            elif (l == c):
+                print(matriz[l][c], end="\t")
             else:
                 print(" \t", end="")
-            
         print()
-        
-numConvidados = int(input('Quantos convidados? '))
+
+
+n = int(input('Quantos convidados? '))
 # numMesas =  int(input('Quantas mesas? '))
 # limMin =  int(input('Qual o mínimo de pessoas que devem sentar em cada mesa? '))
 # limMax =  int(input('E qual o máximo? '))
 
-criarMatriz()
+matriz = criarMatriz(n)
+printMatriz(matriz)
